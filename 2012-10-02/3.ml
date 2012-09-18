825eed6106d2210733a605173f8ee2bd1a902d71
Kazuhiko Sakaguchi <pi8027@gmail.com>
2012-09-19 03:03:21 +0900

    2012-10-02

--- 2012-10-02/3.ml
+++ 2012-10-02/3.ml
@@ -1,6 +1,10 @@
 let rec count_lines_ch (file : in_channel) : int =
   try
-    let _ = input_line file in succ (count_lines_ch file)
+    input_line file;
+    succ (count_lines_ch file)
   with End_of_file -> 0
 
-let count_lines (filename : string) : int = count_lines_ch (open_in filename);;
+let count_lines (filename : string) : int =
+  let file = open_in filename in
+  let r = count_lines_ch file in
+  close_in file ; r
