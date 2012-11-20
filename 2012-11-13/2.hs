2a536cf2a4fc19c93ad7f46d1efa4e62e8ff56df
Kazuhiko Sakaguchi <pi8027@gmail.com>
2012-11-20 17:50:14 +0900

    2012-11-13

--- 2012-11-13/2.hs
+++ 2012-11-13/2.hs
@@ -1,11 +1,11 @@
 -- 1
-pyth :: Num a => (a, a, a) -> Bool
+pyth :: (Num a, Eq a) => (a, a, a) -> Bool
 pyth (x, y, z) =
     let x' = x*x
         y' = y*y
         z' = z*z in
-    x' == y'+z' || y' == x'+z' || z' == x'+y'
+    (x' == y'+z' || y' == x'+z' || z' == x'+y') && x /= 0 && y /= 0 && z /= 0
 
 -- 2
-triads :: (Num a, Enum a) => a -> [(a, a, a)]
-triads n = filter pyth [(x, y, z) | x <- [0..n], y <- [0..n], z <- [0..n]]
+triads :: (Num a, Enum a, Eq a) => a -> [(a, a, a)]
+triads n = filter pyth [(x, y, z) | x <- [0..n], y <- [x..n], z <- [y..n]]
